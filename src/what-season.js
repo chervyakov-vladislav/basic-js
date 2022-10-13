d0bdde8758a7c27e1cca6bc398d240665e57a337
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-10-13 14:35:50 +0700

    feat: what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if ( isNaN(Date.parse(date).toString()) || typeof date === 'number') throw new Error('Invalid date!');
+  if (Object.getOwnPropertyNames(date).length > 0)  throw new Error('Invalid date!');
+
+  let month = date.getMonth();
+
+    if (month > 1 && month < 5) return 'spring'
+    if (month > 4 && month < 8) return 'summer'
+    if (month > 7 && month < 11) return 'autumn'
+    return 'winter'
 }
 
 module.exports = {
   getSeason
 };
+
+// console.log(getSeason(new Date(2020, 02, 31)))
+
+
+// const deeperFakeDate = {
+//   toString() {
+//       return Date.prototype.toString.call(new Date());
+//   },
+//   getMonth() {
+//       return Date.prototype.getMonth.call(new Date());
+//   },
+//   getFullYear() {
+//       return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+//   },
+//   getDate() {
+//       return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+//   },
+//   getHours() {
+//       return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+//   },
+//   getMinutes() {
+//       return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+//   },
+//   getSeconds() {
+//       return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+//   },
+//   getMilliseconds() {
+//       return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+//   },
+//   getDay() {
+//       return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// console.log(deeperFakeDate);
+
+// console.log(getSeason(deeperFakeDate));
